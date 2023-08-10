b48d471db207149f5cd15bf5e49ff2e42d71e5b3
lelydelvia <lely2sipayung@gmail.com>
2023-08-10 11:46:27 +0700

    "test"

--- apps/app.js
+++ apps/app.js
@@ -13,11 +13,11 @@ app.disable("x-powered-by");
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/app1', (req, res) => {
-  res.send('Hello this Apps 1!')
+  res.send('Hello guys this Apps 1!')
 });
 
 app.get('/app2', (req, res) => {
-  res.send('Hello this App 2!')
+  res.send('Hello guys this App 2!')
 });
 
 app.get('/users', (req, res, next) => {
